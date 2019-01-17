1347e09c8355a3c4959050ababbd9d6750a9dc62
Ben Parisot <me@benparisot.com>
2019-01-16 19:57:11 -0800

    update js file

--- main.js
+++ main.js
@@ -2,3 +2,14 @@ console.log('hello console!', 7);
 
 var sum = 0;
 sum;
+
+var x = 21;
+x++;
+console.log(x);
+
+if(x > 22) {
+    console.log('thats a big number!');
+}
+else {
+    console.log('sorry babe thats a tiny little number');
+}
